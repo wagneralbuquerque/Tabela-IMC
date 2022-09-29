91184b5cbfde6f7ee2e855bbcf7a21f895ce6048
Wagner <wagneralbuquerque2010@gmail.com>
2022-09-29 10:38:09 -0300

    code

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -34,17 +34,11 @@ function montaTr(paciente) {
   var pacienteTr = document.createElement("tr");
   pacienteTr.classList.add("paciente");
 
-  var pesoTd = montaTd(paciente.peso, "info-peso");
-  var alturaTd = montaTd(paciente.altura, "info-altura");
-  var nomeTd = montaTd(paciente.nome, "info-nome");
-  var gorduraTd = montaTd(paciente.gordura, "info-gordura");
-  var imcTd = montaTd(paciente.imc, "info-imc");
-
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  pacienteTr.appendChild(imcTd);
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
   return pacienteTr;
 }
